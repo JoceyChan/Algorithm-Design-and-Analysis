76c37b4f379a0fcd08d4aabb4d9068643cc05707
Jocelyn Chan <joceychan@jocelyns-mbp.home>
2020-09-16 11:38:47 -0700

    Completed

--- Lab02/jchan234.cpp
+++ Lab02/jchan234.cpp
@@ -2,12 +2,43 @@
 #include <iostream>
 
 using namespace std;
+//Resources used: Book, Lecture, Lab Sessions and Lab guidelines
+
+int binarySearchFn(int nSize, int aOfS, int* array){
+//     // std::cout << 22;
+    int l = 0;
+    int r = nSize - 1;
+    while(l < r || l == r){
+        int half = (l + r) / 2;
+        if(array[half] == aOfS){
+            std::cout << half;
+            return half;
+        }
+        else if(array[half] > aOfS){
+            r = half - 1;
+        }
+        else{
+            l = half + 1;
+        }
+    }
+    std::cout << -1;
+    return -1;
+
+}
 int main(){
-    int n;
+    int nSize;
     int aOfS;
-    std::cin >> n;
+    std::cin >> nSize;
     std::cin >> aOfS; 
 
     int* array;
-    array = new int[n];
+    array = new int[nSize];
+    for(int i = 0; i < nSize; i++){
+        int aI;
+        std::cin >> aI;
+        array[i] = aI;
+    }
+    // return -1;
+    binarySearchFn(nSize, aOfS, array);
+    return 0;
 }
