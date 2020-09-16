f6afe3ac1ef12797badbf53f87771987a99e974f
Jocelyn Chan <joceychan@jocelyns-mbp.home>
2020-09-15 22:37:21 -0700

    created a main method

---
+++ Lab02/jchan234.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+int main(){
+    int n;
+    int aOfS;
+    std::cin >> n;
+    std::cin >> aOfS; 
+
+    int* array;
+    array = new int[n];
+}
