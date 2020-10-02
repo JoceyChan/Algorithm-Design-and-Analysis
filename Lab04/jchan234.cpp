6a0df3a57f8de97c1435dc7988625564bb90e3cd
Jocelyn Chan <joceychan@jocelyns-mbp.home>
2020-10-01 23:51:15 -0700

    Completed

--- Lab04/jchan234.cpp
+++ Lab04/jchan234.cpp
@@ -3,59 +3,77 @@
 //Resources used: Lecture/Book/Lab Session
 using namespace std;
 
-int max_crossing(int* array, int low, int mid, int high){
-    int left_sum = INT_MIN;
-    int sum = 0; 
-    //finish..
-    for(int i = mid; i > low - 1; --i){
-        sum += array[i];
-        if(sum > left_sum){
-            left_sum = sum;
-            int maxLeft = i; 
-        }
-    }
-    int right_sum = INT_MIN;
+int maxCrossingFn(int* array, int low, int mid, int high){
+    int rightSum = INT_MIN;
     int sum2 = 0; 
-    for(int j = mid + 1; j < high + 1; ++j){
+    //Finish..
+    int cond2 = high + 1;
+    for(int j = mid + 1; j < cond2; ++j){
         sum2 += array[j];
-        if(sum2 > right_sum){
-            right_sum = sum2;
+        if(sum2 > rightSum){
+            rightSum = sum2;
             int maxRight = j;
         }
     }
+    int leftSum = INT_MIN;
+    int sum = 0; 
+    int cond = low - 1; 
     //finish..
+    for(int i = mid; i > cond; --i){
+        sum += array[i];
+        if(sum > leftSum){
+            leftSum = sum;
+            int maxLeft = i; 
+        }
+    }
     
-    return left_sum + right_sum;
+    return leftSum + rightSum;
 }
-int max_subarray(int* array, int low, int high){
+
+// int maxHelper(int* array, int low, int high, int sumL, int mid, int leftSum , int rightSum, int crossSum){
+//     if(leftSum >= rightSum && leftSum >= crossSum){
+//         return leftSum;
+//     }
+//     else if( crossSum <= rightSum && leftSum <= rightSum){
+//         return rightSum;
+//     }
+//     else{
+//         return crossSum;
+//     }
+// }
+
+int maxSubarray(int* array, int low, int high){
     if(low == high){
         return array[low];
     }
-    int mid = (low +high) /2;
-    int left_sum = max_subarray(array, low, mid);
-    int right_sum = max_subarray(array, mid+1, high);
-    int cross_sum = max_crossing(array, low, mid, high);
-    if(left_sum >= right_sum && left_sum >= cross_sum){
-        return left_sum;
+    int sumL = (low + high);
+    int mid = sumL /2;
+    int leftSum = maxSubarray(array, low, mid);
+    int rightSum = maxSubarray(array, mid+1, high);
+    int crossSum = maxCrossingFn(array, low, mid, high);
+    
+    
+    if(crossSum <= rightSum && leftSum <= rightSum){
+        return rightSum;
     }
-    else if(left_sum <= right_sum && cross_sum <= right_sum){
-        return right_sum;
+    else if(leftSum >= rightSum && leftSum >= crossSum){
+        return leftSum;
     }
     else{
-        return cross_sum;
+        return crossSum;
     }
-    //finish..
 }
 
 int main(){
-    int n;
-    cin >> n;
+    int nSize;
+    std::cin >> nSize;
     int* array;
-    array = new int[n];
-    for(int i = 0; i < n; i++){
-        cin >> array[i];
+    array = new int[nSize];
+    for(int i = 0; i < nSize; i++){
+        std::cin >> array[i];
     }
-    int max_sum = max_subarray(array, 0, n-1); //maybe just n not n-1
+    int length = nSize-1;
+    int max_sum = maxSubarray(array, 0, length); //maybe just n not n-1
     std::cout << max_sum;
 
     return 0;
