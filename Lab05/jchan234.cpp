b9dd6564bb396aeda17c022d134a1fe0fbe53c74
Jocelyn Chan <joceychan@jocelyns-mbp.home>
2020-10-05 22:52:24 -0700

    WIP

--- Lab05/jchan234.cpp
+++ Lab05/jchan234.cpp
@@ -1,35 +1,42 @@
 #include <stdio.h>
 #include <iostream>
 //Resources used: Lecture/Book/Lab Session
+
 using namespace std;
+
 void maxHeapify(int* array, int heapSize, int i){ 
     int largest = i;
-    int left = 2 * i + 1;
-    int right =  2 * i + 2;
+    int multi = 2 * i;
+    int left = multi + 1;
+    int right =   multi + 2;
 
-    if(left < heapSize && array[left] > array[largest]){
-        largest = left;
-    }
     if(right < heapSize && array[right] > array[largest]){
         largest = right;
     }
+    if(left < heapSize && array[left] > array[largest]){
+        largest = left;
+    }
+    
     if(largest != i){
         swap(array[i], array[largest]);
         maxHeapify(array, heapSize, largest);
     }
 
 }
-// void buildHeap(int* array, int heapSize){
-//     for(int i = heapSize/2; i >= 1; i--){
-//         maxHeapify(array, heapSize, i);
-//     }
-// }
-void heapSort(int* array, int heapSize){
-    // buildHeap(array, heapSize);
-    for(int i = heapSize/2-1; i >= 0; i--){
+void buildHeap(int* array, int heapSize){
+    int subs = 2 - 1;
+    int div = heapSize/subs;
+    for(int i = div; i >= 0; i--){
         maxHeapify(array, heapSize, i);
     }
-    for(int i = heapSize-1; i >= 0; i--){
+}
+void heapSort(int* array, int heapSize){
+    buildHeap(array, heapSize);
+    // for(int i = heapSize/2-1; i >= 0; i--){
+    //     maxHeapify(array, heapSize, i);
+    // }
+    int minu = heapSize - 1;
+    for(int i = minu; i >= 0; i--){
         swap(array[0], array[i]);
         maxHeapify(array, i, 0);
     }
@@ -38,7 +45,8 @@ int main(){
     int heapSize;
     std::cin >> heapSize;
     int* array;
-    array = new int[heapSize+1];
+    int len = heapSize + 1;
+    array = new int[len];
 
     for(int i = 0; i < heapSize; i++){
         std::cin >> array[i];
