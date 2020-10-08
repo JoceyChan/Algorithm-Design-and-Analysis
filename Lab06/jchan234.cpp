168d5c12bc30c5f492140d1c96e61141de812bc1
Jocelyn Chan <joceychan@jocelyns-mbp.home>
2020-10-07 22:45:39 -0700

    WIP

--- Lab06/jchan234.cpp
+++ Lab06/jchan234.cpp
@@ -1,36 +1,45 @@
-#include <stdio.h>
 #include <iostream>
-#include <time.h>
+#include <climits>
 //Resources used: Lecture/Book/Lab Session
 using namespace std;
 
-int partition(int* A, int p, int r){  
-    int x = A[r];  
+int partition(int* array, int l, int r){  
+    int x = array[r];  //pivot point
     // finish
+    for(int i = l; i <= r; i++){
+        if(array[i] < x){
+            swap(array[i], array[l]);
+            l++;
+        }
+    }
+    swap(array[r], array[l]);
+    return l;
 }
-int rpartition(int* A, int p, int r){  
-    int i = rand()%(r - p + 1) + p;  
-    swap(A[r], A[i]);  
-    return partition(A, p, r);
+int rPartition(int* array, int l, int r){ 
+    int moving = (r - l + 1);
+    int i = rand()% moving + l;  //generater random num between left and right meaning smaller and greater
+    swap(array[r], array[i]);  
+    return partition(array, l, r);
 }
-void quicksort(int* A, int p, int r){  
-    if (p < r){    
-        int q = rpartition(A, p, r);
-        quicksort(A, p, q-1);
-        quicksort(A, q+1, p);  
+void quicksort(int* array, int l, int r){  
+    if (l < r){    
+        int q = rPartition(array, l, r);
+        quicksort(array, l, q-1); //sep sort left
+        quicksort(array, q+1, r);  //sep sort right
     }
 }
 int main(){  
-    srand(time(NULL));  
-    int n = 0;  
-    cin>>n;  
-    int* arr = new int[n];  
+    // srand(time(NULL));  
+    int n;  
+    std::cin>>n;  
+    int* array;
+    array  = new int[n];  
     for(int i = 0; i < n; i++){    
-        cin>>arr[i];  
+        std::cin>>array[i];  
     }  
-    quicksort(arr, 0, n-1);  
+    quicksort(array, 0, n-1);  
     for(int i = 0; i < n; i++){    
-        cout<<arr[i]<<";";  
+        std::cout<<array[i]<<";";  
     }  
     return 0;
 }
