247c22f78b06d7b9046dbf0179d01f54078c73db
Jocelyn Chan <joceychan@Jocelyns-MBP.attlocal.net>
2020-10-14 23:35:07 -0700

    WIP

--- Lab07/jchan234.cpp
+++ Lab07/jchan234.cpp
@@ -1,60 +1,67 @@
-#include <iostream>
+#include "iostream"
 #include <vector>
 
+
 using namespace std;
-
-vector<vector<int>> counting_sort(vector<vector<int>> A, int c, int n){  
-    int counts[10] = {0};  
+// for i = 1 to d --> use countingSort to sort array A tto digit i
+vector<vector<int>> countingSort(vector<vector<int>> A, int sigIndex, int n){  
+    // count all the items
+    int counts[10]; 
     // first for loop here... 
-    vector<vector<int>> arr[c];  //output array of vector 
-    vector<vector<int>>* results = new vector<vector<int>>[c];       
-
-    for(int i = 0; i < c; i++){
-        counts[A[i][n]%10]++;  
+    for(int i = 0; i < 10; i++){
+        counts[i] = 0;
     }
+
     // second for loop here:  
-    for(int j=1; j < 10; j++){    
-        counts[j] += counts[j-1];
-    }  
+    for(int j = 1; j < n; j++){    
+        counts[A[j][sigIndex]]++; 
+        // std::cout << counts[A[j][sigIndex]] << "," << A[j][sigIndex] << ";" << endl;  
+    }
     // third for loop here...
-    for(int z = c - 1; z >= 0; z--){    
-        results[counts[A[z][n]%10]-1] = A[z];
-    }   
-    // the last for loop 
-    for(int a = 0; a < c; a++){
-            A[a] = results[a];
-        } 
+    for(int i = 1; i < 10; i++){
+        counts[i] += counts[i-1]; //cummulative sum
+    }
     vector<vector<int>> B = A;   
-    return B;
+    // the last for loop 
+    for(int j = n - 1; j >= 0; j--){
+        B[counts[A[j][sigIndex]]-1] = A[j];
+        counts[A[j][sigIndex]]--;
+    }
+     return B;
 }
     
 vector<vector<int>> radix_sort(vector<vector<int>> A, int n){  
     int k = 9;  
     vector<vector<int>> B = A;  
-    for(int i = k; i >= 0; i--){   
-        B = counting_sort(B, i, n);  
-    }  
+    for(int i = k; i >= 0; i--){  
+        B = countingSort(B, i, n); 
+        // std::cout << i << ";" << endl;  
+    }
+    // std::cout << "made it this far" << ";"; 
+    // exit (EXIT_FAILURE);  
     return B;
 }
-int main (){    
+int main() {    
     int arraySize;    
-    std::cin >> arraySize;        
+    std::cin >> arraySize;
     vector<vector<int>> Sequence;        
     for (int i = 0; i < arraySize;i++){        
         vector<int> row;        
-        for (int j = 0; j < 10;j++){            
+        for (int j = 0; j < 10;j++){    
             int value;            
-            std::cin>>value;            
-            row.push_back(value); //values added to array       
+            std::cin>>value;
+            row.push_back(value); //values added to array    
         }        
         Sequence.push_back(row);    
-    }        
-    vector<vector<int>> sorted = radix_sort(Sequence, arraySize);    
-    //print out sorted array    
-    for(int i = 0; i < arraySize; i++){
-        for(vector<int>::iterator output = A[i].begin(); output != A[i].end(); output++){
-            cout << *output << ";";
-        }
     }
-return 0;
+
+    vector<vector<int>> sorted = radix_sort(Sequence, arraySize); 
+    //print out sorted array 
+    for(int i = 0; i < sorted.size() - 1; i++){
+        for(int j = 0; j < 10; j++){
+            std::cout << sorted[i][j] << ";";
+        }
+        std::cout << endl;
+    }
+    return 0;
 }
