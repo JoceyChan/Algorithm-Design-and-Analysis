4702dfcfc3129678e73c1577159d97daa6e4da76
Jocelyn Chan <joceychan@jocelyns-mbp.home>
2020-11-16 15:18:40 -0800

    WIP

---
+++ Lab12/jchan234.cpp
@@ -0,0 +1,18 @@
+#include <sstream>
+#include <iostream>
+// #include <string>
+//resources used Textbook, lab session
+
+using namespace std;
+void cutRod(int *p, int n){ //p = price, n = size
+    if(n == 0){
+        return;
+    }
+    int q = INT_MIN;
+}
+void memoizedCutRod(){
+
+}
+int main(){
+
+}
